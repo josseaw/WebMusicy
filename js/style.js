fa5df2093e1d9b844c9dcbefeaca4579964288fc
josseandriyanto <jossea.w.55@gmail.com>
2022-07-21 11:29:46 +0700

    Day 10

--- js/style.js
+++ js/style.js
@@ -1,13 +1,19 @@
 $(document).ready(function(){
-    $('.slide-album').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3
-    });
-    $('.list-event').slick({
-        infinite: true,
-        arrow: false,
-        slidesToShow: 1,
-        slidesToScroll: 1
-    });
+    // $('.slide-album').slick({
+    //     infinite: true,
+    //     slidesToShow: 3,
+    //     slidesToScroll: 3
+    // });
+    // $('.list-event').slick({
+    //     infinite: true,
+    //     arrow: false,
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1
+    // });
+    // $('.slide-news').slick({
+    //     infinite: true,
+    //     arrow: false,
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1
+    // });
 });
