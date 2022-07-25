81ee43540e03b09e3373ce341005a19a929953f8
josseandriyanto <jossea.w.55@gmail.com>
2022-07-25 11:31:52 +0700

    Day 12

--- js/style.js
+++ js/style.js
@@ -1,15 +1,28 @@
-$(document).ready(function(){
+$(document).ready(function() {
+    // button open and close
+    $('.button').on('click', function() {
+        $(this).css({ "display": "none" });
+        $('.close').css({ "display": "block" });
+        $('nav .links').slideDown(300);
+        $('nav .login').slideDown(500);
+    });
+    $('.close').on('click', function() {
+        $(this).css({ "display": "none" });
+        $('.button').css({ "display": "block" });
+        $('nav .links').slideUp(500);
+        $('nav .login').slideUp(300);
+    });
     // $('.slide-album').slick({
     //     infinite: true,
     //     slidesToShow: 3,
     //     slidesToScroll: 3
     // });
-    // $('.list-event').slick({
-    //     infinite: true,
-    //     arrow: false,
-    //     slidesToShow: 1,
-    //     slidesToScroll: 1
-    // });
+    $('.list-event').slick({
+        infinite: true,
+        // arrow: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
     // $('.slide-news').slick({
     //     infinite: true,
     //     arrow: false,
